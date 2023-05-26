2607907d1272058f3da112d4d0a046ef704cd059
Akash Zinzurde <134063354+AkashZinzurde@users.noreply.github.com>
2023-05-26 09:48:40 +0530

    C# language
    i  crate this program

---
+++ Add.cs
@@ -0,0 +1,14 @@
+
+ using System;
+ 
+ class Add
+ {
+public static void Main(String [] A)
+{
+    int a=10;
+   int b=30;
+   int sum;
+    sum=a+b;
+    System.Console.WriteLine(sum);
+    }
+ }
---
+++ Demo.cs
@@ -0,0 +1,12 @@
+
+ using System;
+ 
+ class Demo 
+ {
+public static void Main(String [] A)
+{
+    int a=10,b=30,sum;
+    sum=a+b;
+    System.Console.WriteLine("sum");
+}
+ }
